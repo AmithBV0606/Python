8c20aaf1256d7c186503aea79e031e940e4a34fc
AmithBV0606 <amithrao0606@gmail.com>
2024-11-12 07:45:03 +0530

    Statements, loops, functions, exception handling and file handling.

--- 12_Statements.py
+++ 12_Statements.py
@@ -11,9 +11,9 @@ else:
     print("YES, you can vote!")
 
 # elif statement
-a = 33
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
