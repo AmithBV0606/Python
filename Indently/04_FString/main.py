f7901f1e879f43bb41b8f726c1602478521f3c45
Amith B V <amithrao0606@gmail.com>
2025-07-30 18:08:56 +0530

    Indently: F-String

---
+++ Indently/04_FString/main.py
@@ -0,0 +1,10 @@
+# Without F-String
+
+name: str = "Amith"
+age: int = 24
+
+print("My name is " + name + " and I am " + str(age) + " years old!!") # More efforts in writing
+
+# Using F-String
+print(f"My name is {name} and I'm {age} years old!!!")
+print(F"My name is {name} and I'm {age} years old!!!")
