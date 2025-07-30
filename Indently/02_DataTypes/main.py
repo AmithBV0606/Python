11503bdb12d0bcb1795252b186a1e67dfb60eefe
Amith B V <amithrao0606@gmail.com>
2025-07-30 17:54:38 +0530

    Datatypes in Python and TypeConversion.

---
+++ Indently/02_DataTypes/main.py
@@ -0,0 +1,28 @@
+# Strings
+text = 'apple'
+
+# Number
+number = 10
+
+# Float
+decimal = -10.123
+
+# Boolean
+has_money = False
+
+# Tuples : Data cannot be changed(Immutable)
+coordinates = (2.5, 1.5)
+
+# List : Data can be changed(Mutable)
+Fruits = ["Apple", "Mangos", "Bananas"]
+
+# Set : Will not allow duplicate entry
+unique = {1,2,3,4,5,5,5,}
+print(unique)
+
+# Dictionary : Like objects in JavaScript
+users = {
+    'name': "Amith", 
+    'age': 24
+}
+print(users)
---
+++ Indently/02_DataTypes/typeConversion.py
@@ -0,0 +1,10 @@
+# Type conversion : 
+
+value1 = "100"
+print(10 + int(value1))
+
+value2 = 2025
+print("It's year " + str(value2))
+
+# To float : float(value)
+# To set : set(value)
