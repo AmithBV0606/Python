c8e727405ddef23d7c8fedc1a963097dd464db27
Amith B V <amithrao0606@gmail.com>
2025-07-30 18:51:50 +0530

    Indently: Comparison Operators.

---
+++ Indently/07_ComparisonOperators/main.py
@@ -0,0 +1,16 @@
+a: int = 1
+b: int = 2
+c: int = 2
+
+print(f'a > b : {a > b}')
+print(f'a >= b : {a >= b}')
+
+print(f'a < b : {a < b}')
+print(f'a <= b : {a <= b}')
+
+print(f'b > c : {b > c}')
+print(f'b >= c : {b >= c}')
+
+print(f'a == b : {a == b}')
+
+print(f'a != b: {a != b}')
